a905359304cce75286d5592a7e4d89b5429c72dc
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-09-16 09:23:09 +0530

    Create Layout.jsx & implement route in all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <h1 className='bg-red-300'>Hi there</h1>
+    <>
+
+    </>
   )
 }
 
