da22a5985d116194d36e5e0ccd4d200b09bc549a
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-09-16 09:32:21 +0530

    Create  About and Contact route

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 
 
@@ -14,7 +15,8 @@ const router = createBrowserRouter([
     path: '/', element: <Layout />,
     children: [
       { path: "", element: <Home /> },
-      { path: "about", element: <About /> }
+      { path: "about", element: <About /> },
+      { path: 'contact', element: <Contact /> }
     ]
 
   }
